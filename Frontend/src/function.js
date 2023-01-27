0f39e5d20bed25c38d7b6b0aad825f0e767da56c
klara <klara.steinauer@hotmail.com>
2023-01-27 14:20:56 +0100

    - added JS to Frontend

---
+++ Frontend/src/function.js
@@ -0,0 +1,29 @@
+$("#createProductButton").on("click", e => {
+    const product = {
+        "name": "Frontend",
+        "description": "Ein Produkt",
+        "price": 20.99,
+        "quantity": 3,
+        "type": "frontend",
+    }
+
+    $.ajax({
+        url: "http://localhost:8080/products",
+        type: "POST",
+        cors: true,
+        contentType: "application/json",
+        data: JSON.stringify(product),
+        success: console.log,
+        error: console.error();
+    });
+});
+
+$("#getProductsButton").on("click", function (e)){
+    $ajax({
+        url: "http://localhost:8080/products",
+        type: "GET",
+        cors: true,
+        success: function (products) { addProductsToPage(products) },
+        error: function (error) { console.error(error) }
+    })
+};
