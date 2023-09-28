f1235672691213706157b7dffb9a4f8d50156f7f
Mais <mayrmichael@gmx.net>
2023-09-28 13:18:35 +0200

    updated active profiles for tests

--- Backend/src/test/java/com/webshop/webshop/WebshopApplicationTest.java
+++ Backend/src/test/java/com/webshop/webshop/WebshopApplicationTest.java
@@ -2,8 +2,10 @@ package com.webshop.webshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebshopApplicationTest {
 
 	@Test
